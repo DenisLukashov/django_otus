c9e8250c26d0103f30923f673037c1d0132f9d34
Denis <den3900@mail.ru>
2020-05-01 19:03:55 +0300

    #2

--- myproj/homepage/urls.py
+++ myproj/homepage/urls.py
@@ -1,8 +1,33 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
 
+
 app_name = 'homepage'
+
+
+class YearConverter:
+    regex = r'[0-9]{4}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value
+
+
+register_converter(YearConverter, 'year')
+
+
 urlpatterns = [
-    path('', views.index_page, name='index'),
-    path('articles/', views.articles, name='articles'),
+    path(
+        '',
+        views.TemplateView.as_view(template_name='homepage/index.html'),
+        name='index'
+    ),
+    path('articles/', views.ArticleView.as_view(), name='articles'),
+    # re_path(
+    #     r'^articles/(?P<year>[0-9]{4})/$',
+    #     views.article_year, name='articles'
+    # ),
+    path('articles/<year:year>/', views.article_year, name='articles_year'),
 ]
--- myproj/homepage/views.py
+++ myproj/homepage/views.py
@@ -1,19 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods, require_GET
+# from django.views.decorators.http import (
+#     require_http_methods,
+#     require_GET
+# )
+from django.views.generic import TemplateView
+
+from datetime import datetime, timedelta
+from random import randint
+
 
 # Create your views here.
 
-@require_http_methods(['GET', 'POST'])
-def index_page(request):
+# class IndexPageView(TemplateView):
+#     template_name = 'homepage/index.html'
 
-    return render(request, 'homepage/index.html')
+#     def get(self, request):
+#         return render(request, self.template_name)
 
-@require_GET
-def articles(request):
-    args = {
-        'articles': list(range(1,10)),
-        'val1': '<h3>Value1</h3>',
-        'val2': '<h4>Value2</h4>'
-    }
-    return render(request, 'homepage/articles.html', args)
+
+
+
+
+class ArticleView(TemplateView):
+    template_name = 'homepage/articles.html'
+
+    def get_dates_list(self, count=10):
+        result = []
+        today = datetime.today()
+        for i in range(count):
+            date = today - timedelta(days=i)
+            for j in range(randint(1, 4)):
+                result.append(
+                    date.replace(hour=randint(0, 23))
+                )
+        return result
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        my_obj = MyClass()
+        my_obj.data = {'spam': 'eggs'}
+        my_obj.list = list(range(10, 20))
+
+        args = {
+            'articles': list(range(1, 10)),
+            'val0': 'abs',
+            'val1': 'OTUS',
+            'val2': '<h4>Value2</h4>',
+            'obj': my_obj,
+            'a_title': 'django and sublime text 3',
+            'string': (
+                'First line\n'
+                'Second line\n'
+                'Third line\n'
+            ),
+            'current': datetime.today,
+            'dates': self.get_dates_list(),
+            'items': list(range(4)),
+        }
+        context.update(args)
+        return context
+
+
+
+def article_year(request, year):
+    return HttpResponse(f'<h1>Year is {year} ({type(year)})</h1>')
+
+
+class MyClass:
+
+    foo = 42
+    bar = 60
+
+    def __repr__(self):
+        return f'<MyClass {self.foo} {self.bar}>'
