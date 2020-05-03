37a6dc3f367248deb80e6da8f1361cca701996b3
Denis_L <den3900@mail.ru>
2020-05-03 23:03:09 +0300

    #3.2

--- myproj/authors/forms.py
+++ myproj/authors/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from authors.models import Author
 
 
-class AuthorForm(forms.Form):
+class AuthorForm(forms.ModelForm):
     """AuthorForm definition."""
 
     class Meta:
 
         model = Author
-        fields = ('first_name', 'last_name', 'email')
+        # fields = ('first_name', 'last_name', 'email',)
+        exclude = ('level',)
--- myproj/authors/views.py
+++ myproj/authors/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import TemplateView, ListView
 from .models import Author
 from .forms import AuthorForm
@@ -13,15 +13,6 @@ class IndexView(ListView):
 
     model = Author
     context_object_name = 'authors'
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['authors'] = Author.objects.all()
-    #     return context
-
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     queryset = queryset.filter(email__endswith='otus.ru')
-    #     return queryset
 
 
 class CreateAutherView(TemplateView):
@@ -32,4 +23,14 @@ class CreateAutherView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['form'] = AuthorForm()
         return context
+        
+    def post(self, request):
+        form = AuthorForm(request.POST)
+        if not form.is_valid():
+            return render(request, self.template_name, {'form': form})
+        new_author = form.save(commit=False)
+        new_author.level = 'J'
+        new_author.save()
+        return redirect(reverse('index'))
+        
 
